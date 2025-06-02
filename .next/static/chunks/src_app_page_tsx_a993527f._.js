6a9c1f26bd00db3593dfbb22d710885f38dedc50
artevoprints <artevoprints@gmail.com>
2025-06-02 16:14:14 +0200

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1748873651830

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_09f05ddd._.js"
+],
+    source: "dynamic"
+});
